d3f286b6d4327f09bf1c26e97e5ea34ee983b24c
Ankush Rana <90180315+ankushrana981@users.noreply.github.com>
2024-07-16 17:47:52 +0530

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -23,8 +23,8 @@ mongoose.connect(process.env.MONGO_URI!).then(()=>{
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended:false}))
-app.get("/", (req,res)=> res.send("Hello World"));
 
+app.use("/", (req,res)=> res.send("Hello World"));
 app.use('/api', foodRouter);
 app.use('/api/user', userRouter);
 app.use('/api/orders', orderRouter);
