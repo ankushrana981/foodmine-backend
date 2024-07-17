09c73cbec879cb0969304a36f4b69c0ed697ecf1
Ankush <ankushrana98145@gmail.com>
2024-07-17 12:55:21 +0530

    fix build issue

--- src/server.ts
+++ src/server.ts
@@ -22,7 +22,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended:false}))
 
-app.use("/", (req,res)=> res.send("Hello World"));
+// app.use("/", (req,res)=> res.send("Hello World"));
 app.use('/api', foodRouter);
 app.use('/api/user', userRouter);
 app.use('/api/orders', orderRouter);
